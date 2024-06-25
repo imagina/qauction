78aff80fdffe03596f651a62b5a65e60d6dfb6f6
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-25 18:02:15 -0500

    + added translations

---
+++ _i18n/es/index.js
@@ -0,0 +1,31 @@
+export default {
+  "iauctions": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Licitaciones",
+        "adminCategories": "Categorias",
+        "adminAuctions": "Licitaciones",
+        "adminBids": "Pujas"
+      },
+      "newCategory": "Nueva Categoría",
+      "updateCategory": "Actualizar Categoría",
+      "newAuction": "Nueva licitación",
+      "updateAuction": "Editar licitación",
+      "newBid": "Nueva Puja",
+      "updateBid": "Actualizar Puja",
+      "auctionForm": "Formulario para Licitación",
+      "bidForm": "Formulario para Puja",
+      "activeState": "Activo",
+      "inActiveState": "Inactivo",
+      "finishedState": "Culminado",
+      "typeOpen": "Abierto",
+      "typeInverse": "Inverso",
+      "bidUp": "Pujar",
+      "auction": "Licitación | Licitaciones",
+      "bid": "Puja | Pujas",
+      "setAsWinner": "Marcar como Ganador",
+      "existBid": "Ya tienes una puja para esta licitación",
+      "description": "Descripción"
+    }
+  }  
+}
